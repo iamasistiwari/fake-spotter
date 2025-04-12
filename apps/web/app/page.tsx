9eab7310a895210a8e0f7cffb66d65de6b566218
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-04-12 13:19:16 +0530

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,44 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
+export default function page() {
   return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turbo.build/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="bg-white min-h-screen text-black">
+      <nav className="h-16 border-b border-neutral-300 justify-between items-center flex px-24">
+        <div className="">
+          <span className="text-xl">Fake Spotter</span>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turbo.build?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turbo.build →
-        </a>
-      </footer>
+        <div className="flex space-x-10 font-medium text-neutral-600 items-center justify-center">
+          <Link href={`/contact`}>Scanner</Link>
+          <Link href={`/contact`}>About</Link>
+          <Link href={`/contact`}>Contact</Link>
+          <Link
+            className="border py-2 rounded-md font-normal hover:mb-1 text-black hover:text-blue-500 border-neutral-400 transition-all duration-200 px-6"
+            href={`/contact`}
+          >
+            SIGN IN
+          </Link>
+        </div>
+      </nav>
+
+      {/* Hero section */}
+      <section className="min-h-screen flex justify-between pl-24 pr-4 mt-10">
+        <div>
+          <span>Detect deepfakes before they exploit</span>
+        </div>
+
+        <div className="relative w-[650px] h-96 rounded-4xl overflow-hidden">
+          <Image src="/img2.webp" alt="img1" fill className="object-cover" />
+
+          {/* Gradient overlay at the corner */}
+          <div className="absolute inset-0 bg-gradient-to-br from-neutral-100 via-transparent to-transparent pointer-events-none" />
+          {/* <div className="absolute  inset-0 bg-gradient-to-bl from-neutral-100 via-transparent to-transparent pointer-events-none" /> */}
+          <div className="absolute  inset-0 bg-gradient-to-tr from-neutral-100 via-transparent to-transparent pointer-events-none" />
+
+          {/* <div className="absolute  inset-0 bg-gradient-to-tl from-neutral-100 via-transparent to-transparent pointer-events-none" /> */}
+        </div>
+      </section>
     </div>
   );
 }
